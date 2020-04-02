297d78147d11238b3ddbeef7a519175775397cca
hxc <173315279@qq.com>
2020-04-02 23:20:47 +0800

    fuck

--- day4/function/main.go
+++ day4/function/main.go
@@ -9,6 +9,9 @@ func main() {
 	sum2 := intSumTwo(x, y)
 	fmt.Println(sum, sum2)
 	sayHello()
+
+	sumThree := intSumThree(1, 2, 3, 5, 6)
+	fmt.Println(sumThree)
 }
 
 //func 函数名(参数)(返回值){
@@ -26,4 +29,16 @@ func sayHello() {
 //类型简写
 func intSumTwo(x, y int) int {
 	return x + y
+}
+
+// 变参数是指函数的参数数量不固定。Go语言中的可变参数通过在参数名后加...来标识。
+// 注意：可变参数通常要作为函数的最后一个参数。而且为一个切片
+func intSumThree(x ...int) int {
+	fmt.Println(x) //切片
+	sum := 0
+	for _, v := range x {
+		sum = sum + v
+	}
+	return sum
+	// 本质上，函数的可变参数是通过切片来实现的。
 }
