cd7fc9e4779ba1054a8361a2043305216e5bcc58
hxc <173315279@qq.com>
2020-04-02 14:22:30 +0800

    test

--- day3/map/main.go
+++ day3/map/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+	"sort"
 )
 
 func main() {
@@ -43,10 +44,24 @@ func main() {
 
 	randMap := make(map[string]int, 200)
 
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 10; i++ {
 		keyMap := fmt.Sprintf("key %d", i)
 		value := rand.Intn(100) //生成0~99的随机整数
 		randMap[keyMap] = value
 	}
 	fmt.Println(randMap)
+	//取出map中所有key存入切片
+	var keys = make([]string, 0, 200)
+	for key := range randMap {
+		keys = append(keys, key)
+	}
+	//对切片进行排序
+	sort.Strings(keys)
+
+	//按照排序后的key遍历
+	for _, keyS := range keys {
+		fmt.Println(keyS, randMap[keyS])
+	}
+
+
 }
