5bd157ed18ae61fd627ec8b52aefddbf47654fd9
hxc <173315279@qq.com>
2020-04-03 22:13:37 +0800

    test

--- day4/function2/main.go
+++ day4/function2/main.go
@@ -18,6 +18,9 @@ func main() {
 	fmt.Printf("type of f:%T\n", f)
 	fmt.Println(f(2, 1))
 
+	//函数作为参数
+	g := calc(1, 2222, add)
+	fmt.Println(g)
 }
 
 func add(x, y int) int {
@@ -27,3 +30,10 @@ func add(x, y int) int {
 func sub(x, y int) int {
 	return x - y
 }
+
+//函数作为参数
+func calc(x, y int, op func(int, int) int) int {
+	return op(x, y)
+}
+
+//函数作为返回值
