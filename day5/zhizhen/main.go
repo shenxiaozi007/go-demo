0c5c2fdbdc18fac31a73751f44ba9d54d930a7b5
hxc <173315279@qq.com>
2020-04-07 21:58:44 +0800

    test

--- day5/zhizhen/main.go
+++ day5/zhizhen/main.go
@@ -8,5 +8,25 @@ func main() {
 
 	fmt.Printf("a: %d ptr:%p \n", a, &a)
 	fmt.Printf("b:%p type:%T\n", b, b)
-	fmt.Println(&b)
+	c := &b
+	fmt.Println(c)
+	fmt.Println(*c)
+
+	o := 10
+	x := modify1(o)
+	fmt.Println(o)
+	y := modify2(&o)
+	fmt.Println(x, y)
+	fmt.Println(o)
+
+}
+
+func modify1(x int) int {
+	x = 100
+	return x
+}
+
+func modify2(x *int) int {
+	*x = 100
+	return *x
 }
