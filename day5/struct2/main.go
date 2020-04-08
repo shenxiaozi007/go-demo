2d99ce35b646a76a9b1fa57631d08c220884151e
hxc <173315279@qq.com>
2020-04-08 22:02:24 +0800

    fuck

--- day5/struct2/main.go
+++ day5/struct2/main.go
@@ -10,11 +10,24 @@ type person struct {
 
 func main() {
 	//实例化
-
 	var p1 person
 	p1.name = "nimei"
 	p1.city = "北京"
 	p1.age = 18
 	fmt.Printf("p1 = %v\n", p1)
 	fmt.Printf("p1 = %#v\n", p1)
+
+	//匿名结构体
+	var user struct {
+		Name string
+		Age  int
+	}
+	user.Name = "小丸子"
+	user.Age = 18
+	fmt.Printf("%#v\n", user)
+
+	p2 := new(person)
+	p2.age = 21
+	p2.city = "test"
+	p2.name = "fuck"
 }
