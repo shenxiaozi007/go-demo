8eff57c9ce04d09f2c1629416dcacb4e7c7541d4
hxc <173315279@qq.com>
2020-04-11 17:40:07 +0800

    test

--- day6/interfaceNull/main.go
+++ day6/interfaceNull/main.go
@@ -30,11 +30,41 @@ func main() {
 	}
 	test()
 
-	//
+	//类型断言
+	//想要判断空接口中的值这个时候就可以使用类型断言，其语法格式：x.(T)
+	// x：表示类型为interface{}的变量
+	// T：表示断言x可能是的类型。
+	var xInterface interface{}
+	x1 = "string"
+
+	v, ok := xInterface.(string)
+	if ok {
+		fmt.Println(v)
+	} else {
+		fmt.Println("类型断言失败")
+	}
+
+	switchType(x1)
+	switchType(x)
+	switchType(test)
+
 }
 
 //空接口的应用
 //使用空接口实现可以接收任意类型的函数参数。
 func show(a interface{}) {
 	fmt.Printf("type: %T value:%v", a, a)
+}
+
+func switchType(x interface{}) {
+	switch v := x.(type) {
+	case string:
+		fmt.Printf("x type is a %v \n", v)
+	case int:
+		fmt.Printf("x type is a %v \n", v)
+	case bool:
+		fmt.Printf("x type is a %v \n", v)
+	default:
+		fmt.Printf("其他类型 %v\n", v)
+	}
 }
