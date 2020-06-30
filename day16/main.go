8a03248939b0dcbfa94585572a20bf583af9de77
hxc <173315279@qq.com>
2020-06-30 15:30:28 +0800

    push

--- day16/main.go
+++ day16/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+	"time"
+)
 
 func main() {
 	//每日练习 数组
@@ -24,12 +29,14 @@ func main() {
 		fmt.Println(v,k)
 	}
 	//每日练习 切片 切片是引用类型
+	//注意：通过var声明的零值切片可以在append()函数直接使用，无需初始化。
 
 	var a1 []int
 	//a1 = make([]int, 4)
 	a2 := []string{}
 	a3 := make([]bool, 2)
-
+	fmt.Printf("test--%#v---", a1)
+	fmt.Printf("test--%#v---", a2)
 	//a1[3] = 1
 	a1 = append(a1, 1)
 	a2 = append(a2,"test")
@@ -40,5 +47,58 @@ func main() {
 	a4[0] = true
 	fmt.Println(a1,a2,a3,a4)
 
+	//每日练习 map[KeyType]ValueType
+	//map是引用类型 需要make初始化后才可以使用
+
+	var map1 map[int]string
+	fmt.Printf("%#v---\n", map1)
+	map1 = make(map[int]string, 10)
+	map1[1] = "3232"
+
+	fmt.Printf("%#v---\n", map1)
+	//
+
+	map2 := make(map[string][]string)
+
+	mapSlice := []string{"1", "2"}
+	map2["test"] = mapSlice
+
+	fmt.Printf("%#v---\n", map2)
+
+	fmt.Printf("---%#v---\n", map2["test2"])
+	//判断map2是否存在
+	val, ok := map2["test2"]
+	// 如果key存在ok为true,v为对应的值；不存在ok为false,v为值类型的零值
+	if ok {
+		fmt.Println(val)
+	} else {
+		fmt.Println("不存在")
+	}
+
+	//按顺序循环读取
+	mapTest1 := make(map[string]int, 100)
+
+	rand.Seed(time.Now().UnixNano())
+	sliceList := []string{}
+
+	for i := 0; i < 100; i++ {
+		key := fmt.Sprintf("test%02d", i)
+		value := rand.Intn(100)
+		mapTest1[key] = value
+	}
+	fmt.Println(mapTest1)
+	//赋值到slice
+	for key, _ := range mapTest1 {
+		sliceList = append(sliceList, key)
+	}
+	fmt.Println(sliceList)
+	//排序
+	sort.Strings(sliceList)
+	fmt.Println(sliceList)
+	//按排序获取map值
+	for _, val := range sliceList {
+		mapStr := mapTest1[val]
+		fmt.Printf("%v---%v---\n",val, mapStr)
+	}
 
 }
