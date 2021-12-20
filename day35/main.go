d9ad54687d2cfa178b26d14ce8afa90a3629cc25
huanxinchun <173315279@qq.com>
2021-12-20 19:27:05 +0800

    push

--- day35/main.go
+++ day35/main.go
@@ -75,7 +75,7 @@ func main5() {
 //0 zero
 //map 的输出是无序的。
 
-func main() {
+func main6() {
     a := 1
     b := 2
     defer calc("1", a, calc("10", a, b))
@@ -97,3 +97,57 @@ func calc(index string, a, b int) int {
 //程序执行到第五行的时候，同样先执行 calc(“20”,a,b) 输出：20 0 2 2 得到值 2，同样将 calc(“2”,0,2) 延迟执行；
 //
 //程序执行到末尾的时候，按照栈先进后出的方式依次执行：calc(“2”,0,2)，calc(“1”,1,3)，则就依次输出：2 0 2 2，1 1 3 4。
+
+
+
+
+type Myint int
+
+func (i Myint) PrintInt() {
+    fmt.Println(i)
+}
+
+func main7()  {
+    var i Myint = 1
+    i.PrintInt()
+}
+
+//func (i int) PrintInt ()  {
+//    fmt.Println(i)
+//}
+//
+//func main() {
+//    var i int = 1
+//    i.PrintInt()
+//}
+//参考答案及解析：B。基于类型创建的方法必须定义在同一个包内，上面的代码基于 int 类型创建了 PrintInt() 方法，由于 int 类型和方法 PrintInt() 定义在不同的包内，所以编译出错。
+
+type People interface {
+    Speak(string) string
+}
+
+type Student struct {
+
+}
+
+func (Stu *Student) Speak1(think string) (talk string)  {
+    if think == "speak" {
+        talk = "speak"
+    } else {
+        talk = "hi"
+    }
+    return
+}
+
+func (stu1 Student) Speak(think string) (talk string) {
+    think = "11"
+    return
+}
+
+func main()  {
+    var peo People = Student{}
+    think := "speak"
+    fmt.Println(peo.Speak(think))
+}
+
+//编译错误 Student does not implement People (Speak method has pointer receiver)，值类型 Student 没有实现接口的 Speak() 方法，而是指针类型 *Student 实现该方法。
